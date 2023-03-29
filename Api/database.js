68c1ff1cee9738eae0dd2e54ad44a397493d4e6d
cubo1991 <adavidlopezmathez@gmail.com>
2023-03-29 17:46:47 -0300

    URI agregada

--- Api/database.js
+++ Api/database.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose');
+const {
+  DB_USER, DB_PASSWORD
+} = process.env;
 
 
-const URI = 'mongodb://127.0.0.1/cosmicapp'
+const URI =  `mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.s3smhyw.mongodb.net/?retryWrites=true&w=majority `|| 'mongodb://127.0.0.1/cosmicapp'
 mongoose.set('strictQuery', false);
 mongoose.connect(URI)
 .then(db =>console.log("DB is connect"))
