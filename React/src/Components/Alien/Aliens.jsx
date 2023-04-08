f34d1437f9e3967881ef29bf822b172ad68a6d6d
cubo1991 <adavidlopezmathez@gmail.com>
2023-04-08 13:06:31 -0300

    aliens paginados

--- React/src/Components/Alien/Aliens.jsx
+++ React/src/Components/Alien/Aliens.jsx
@@ -1,19 +1,68 @@
-import React, {useEffect} from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { fetchAliens } from '../../Redux/Actions'
-import s from './Aliens.module.css'
+import ReactPaginate from 'react-paginate';
+import { fetchAliens } from '../../Redux/Actions';
+import s from './Aliens.module.css';
 
 export const Aliens = () => {
-    const dispatch = useDispatch();
-    const aliens = useSelector((state) => state.aliens);
-  
-    useEffect(() => {
-      dispatch(fetchAliens());
-    }, []);
-  
-    return (
-     <div className={s.elemento}>
-     <div className="container">
+  const dispatch = useDispatch();
+  const aliens = useSelector((state) => state.aliens);
+  const [itemOffset, setItemOffset] = useState(0);
+  const [currentItems, setCurrentItems] = useState([]);
+  const [pageCount, setPageCount] = useState(0);
+  const [searchTerm, setSearchTerm] = useState(''); // Nuevo estado para el término de búsqueda
+  const itemsPerPage = 6;
+
+  useEffect(() => {
+    dispatch(fetchAliens());
+  }, []);
+
+  useEffect(() => {
+    const endOffset = itemOffset + itemsPerPage;
+    // Filtrar los aliens que coinciden con el término de búsqueda
+    const filteredAliens = aliens.filter(
+      (alien) => alien.Nombre.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    setCurrentItems(filteredAliens.slice(itemOffset, endOffset));
+    setPageCount(Math.ceil(filteredAliens.length / itemsPerPage));
+  }, [itemOffset, itemsPerPage, aliens, searchTerm]); // Agregar searchTerm a las dependencias del useEffect
+
+  const handlePageClick = (event) => {
+    const newOffset = event.selected * itemsPerPage;
+    console.log(`User requested page number ${event.selected}, which is offset ${newOffset}`);
+    setItemOffset(newOffset);
+  };
+
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value); // Actualizar el término de búsqueda cuando cambia el valor del input
+    setItemOffset(0); // Resetear el offset para volver a la primera página
+  };
+
+  return (
+    <div className={s.elemento}>
+      <div className="container">
+        <div className={s.searchContainer}>
+          <input
+            type="text"
+            placeholder="Buscar el alien"
+            value={searchTerm}
+            onChange={handleSearchChange}
+          />
+        </div>
+        <ReactPaginate
+          breakLabel="..."
+          nextLabel="Próximo >"
+          onPageChange={handlePageClick}
+          pageRangeDisplayed={5}
+          pageCount={pageCount}
+          previousLabel="< Anterior"
+          renderOnZeroPageCount={null}
+          containerClassName={s.pagination}
+          pageClassName={s.pageLink}
+          previousLinkClassName={s.pageLink}
+          nextClassName={s.nextLink}
+          activeClassName={s.activeLink}
+        />
         <table className="table table-striped">
           <thead>
             <tr>
@@ -25,7 +74,7 @@ export const Aliens = () => {
             </tr>
           </thead>
           <tbody>
-            {aliens.map((alien) => (
+            {currentItems.map((alien) => (
               <tr key={alien._id}>
                 <td>{alien.Nombre}</td>
                 <td>{alien.Poder}</td>
@@ -36,7 +85,28 @@ export const Aliens = () => {
             ))}
           </tbody>
         </table>
+        <ReactPaginate
+          breakLabel="..."
+          nextLabel="Próximo >"
+          onPageChange={handlePageClick}
+          pageRangeDisplayed={5}
+          pageCount={pageCount}
+          previousLabel="< Anterior"
+          renderOnZeroPageCount={null}
+          containerClassName={s.pagination}
+          pageClassName={s.pageLink}
+          previousLinkClassName={s.pageLink}
+          nextClassName={s.nextLink}
+          activeClassName={s.activeLink}
+        />
       </div>
-      </div>
-    );
-  };
+    </div>
+  );
+   
+  
+  
+  
+  
+  
+  
+};
--- React/src/Components/NavBar/NavBar.jsx
+++ React/src/Components/NavBar/NavBar.jsx
@@ -22,29 +22,26 @@ export const NavBar = () => {
         </div>  
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid">
-        <a className="navbar-brand" href="# ">Mi Sitio</a>
+      <Link to="/" className="navbar-brand" onClick={handleLinkClick}>INICIO</Link>
         <button className="navbar-toggler" type="button" onClick={handleMenuToggle}>
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className={"collapse navbar-collapse justify-content-center" + (isMenuOpen ? " show" : "")} id="navbarNav">
           <ul className="navbar-nav justify-content-evenly w-100">
-            <li className="nav-item">
-            <Link to="/" className="nav-link" onClick={handleLinkClick}>INICIO</Link>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="# " onClick={handleLinkClick}>Últimos Resultados</a>
+                    <li className="nav-item">
+              {/* <a className="nav-link" href="# " onClick={handleLinkClick}>Últimos Resultados</a> */}
             </li>
             <li className="nav-item">
             <Link to="/copas" className="nav-link" onClick={handleLinkClick}>Copas</Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="# " onClick={handleLinkClick}>Historial Cósmico</a>
+              {/* <a className="nav-link" href="# " onClick={handleLinkClick}>Historial Cósmico</a> */}
             </li>
             <li className="nav-item">
             <Link to="/aliens" className="nav-link" onClick={handleLinkClick}>Aliens</Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="# " onClick={handleLinkClick}>Boletín Cósmico</a>
+              {/* <a className="nav-link" href="# " onClick={handleLinkClick}>Boletín Cósmico</a> */}
             </li>
             <li className="nav-item">
             <Link to="/admin" className="nav-link" onClick={handleLinkClick}>Administrador</Link>
