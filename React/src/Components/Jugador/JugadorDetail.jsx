ccc2e824c0408fff28d161928e80aae90ba196fa
cubo1991 <adavidlopezmathez@gmail.com>
2023-07-03 10:19:05 -0300

    Listo para build

--- React/src/Components/Jugador/JugadorDetail.jsx
+++ React/src/Components/Jugador/JugadorDetail.jsx
@@ -1,68 +1,78 @@
-import React, {useEffect} from 'react'
+import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { fetchJugadores, fetchCopas } from '../../Redux/Actions';
-import s from './JugadorDetail.module.css'
-
+import s from './JugadorDetail.module.css';
 
 export const JugadorDetail = () => {
   let dispatch = useDispatch();
-  let {id} = useParams();
+  let { id } = useParams();
   let jugadoresState = useSelector((state) => state.jugadores);
-  let jugador = jugadoresState.filter(jugador => jugador._id === id)
+  let jugador = jugadoresState.filter((jugador) => jugador._id === id);
 
   useEffect(() => {
     dispatch(fetchJugadores());
     dispatch(fetchCopas());
   }, [dispatch]);
 
-console.log(jugador[0])
+  console.log(jugador[0]);
   return (
     <div className={s.contenedor}>
       {jugador.length > 0 ? (
-       <div className="table-responsive">
-           <h2>{jugador[0].nombre}</h2>
-        <table className="table">
-        <tr>
-    <th>Jugadas</th>
-    <th>Victorias</th>
-    <th>Colonias</th>
-    <th>Prom. Colonias</th>
-    <th>Victorias especiales</th>
-    <th>Campañas ganadas</th>
-    <th>Copas ganadas</th>
-    <th>Ataque solitario</th>
-    <th>Defensa solitaria</th>
-  </tr>
-  <tr>
-    <td>{jugador[0].partidas}</td>
-    <td>{jugador[0].victorias}</td>
-    <td>{jugador[0].colonias}</td>
-    <td>{jugador[0].colonias/jugador[0].partidas || 0}</td>
-    <td>{jugador[0].victoriasEspeciales}</td>
-    <td>{jugador[0].campañas}</td>
-    <td>{jugador[0].copas}</td>
-    <td>{jugador[0].ataqueSolitario}</td>
-    <td>{jugador[0].defensaSolitaria}</td>
-    
-    
-  </tr>
-  <tr>
-    
-  </tr>
-
-
-        </table>
-
-       </div>
-        
+        <div className="table-responsive">
+          <h2>{jugador[0].nombre}</h2>
+          <div className={s.scrollableTable}>
+            <table className={s.table}>
+              <thead>
+                <tr>
+                  <th>Categoría</th>
+                  <th>Valor</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>Jugadas</td>
+                  <td>{jugador[0].partidas}</td>
+                </tr>
+                <tr>
+                  <td>Victorias</td>
+                  <td>{jugador[0].victorias}</td>
+                </tr>
+                <tr>
+                  <td>Colonias</td>
+                  <td>{jugador[0].colonias}</td>
+                </tr>
+                <tr>
+                  <td>Prom. Colonias</td>
+                  <td>{jugador[0].colonias / jugador[0].partidas || 0}</td>
+                </tr>
+                <tr>
+                  <td>Victorias especiales</td>
+                  <td>{jugador[0].victoriasEspeciales}</td>
+                </tr>
+                <tr>
+                  <td>Campañas ganadas</td>
+                  <td>{jugador[0].campañas}</td>
+                </tr>
+                <tr>
+                  <td>Copas ganadas</td>
+                  <td>{jugador[0].copas}</td>
+                </tr>
+                <tr>
+                  <td>Ataque solitario</td>
+                  <td>{jugador[0].ataqueSolitario}</td>
+                </tr>
+                <tr>
+                  <td>Defensa solitaria</td>
+                  <td>{jugador[0].defensaSolitaria}</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
       ) : (
         <p>Cargando...</p>
       )}
     </div>
-  )
-}
-
-
-
-
+  );
+};
